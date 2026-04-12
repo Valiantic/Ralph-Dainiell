3b98a50e91701e8fc22e709ad292864b02d92ba3
Ralph Dainiell Gonzaga <gonzagaralphdainiell@gmail.com>
2026-04-12 18:40:26 +0800

    e100

--- app/components/ProfileHero.tsx
+++ app/components/ProfileHero.tsx
@@ -164,8 +164,8 @@ export const ProfileHero = ({ data }: ProfileHeroProps) => {
                         borderRadius: '14px', textDecoration: 'none',
                         background: '#1877F2'
                     }}>
-                        <svg width="20" height="20" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg" style={{ flexShrink: 0 }}>
-                            <path d="M24 12.073C24 5.404 18.627 0 12 0S0 5.404 0 12.073C0 18.1 4.388 23.094 10.125 24v-8.437H7.078v-3.49h3.047V9.41c0-3.025 1.791-4.697 4.533-4.697 1.313 0 2.686.236 2.686.236v2.971h-1.513c-1.491 0-1.956.93-1.956 1.886v2.267h3.328l-.532 3.49h-2.796V24C19.612 23.094 24 18.1 24 12.073z"/>
+                        <svg width="20" height="20" viewBox="0 0 10 18" fill="white" xmlns="http://www.w3.org/2000/svg" style={{ flexShrink: 0 }}>
+                            <path d="M6.5 3H9V0H6.5C4.57 0 3 1.57 3 3.5V5H1V8H3V18H6V8H8.5L9 5H6V3.5C6 3.22 6.22 3 6.5 3Z"/>
                         </svg>
                         <span style={{ fontSize: '14px', fontWeight: 700, color: '#fff', flex: 1 }}>Facebook</span>
                     </Link>
