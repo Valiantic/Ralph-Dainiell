665d9abfd2f63953d07aaf802df1da219d5ae4de
Ralph <gonzagaralphdainiell@gmail.com>
2026-06-13 15:58:28 +0800

    fix template id

--- app/components/RecommendationSection.tsx
+++ app/components/RecommendationSection.tsx
@@ -78,7 +78,7 @@ const handleSend = async () => {
         imageUrls = await Promise.all(
           images.map(async (img) => {
             const formData = new FormData();
-            formData.append("key", "YOUR_IMGBB_API_KEY");
+            formData.append("key", "f638144d3fc87fd096b0e48eee7e1597");
             formData.append("image", img.base64.split("base64,")[1]);
             const res = await fetch("https://api.imgbb.com/1/upload", {
               method: "POST",
@@ -103,7 +103,7 @@ const handleSend = async () => {
       setIsSuccess(true);
       setText("");
       setImages([]);
-      setTimeout(() => { setIsSuccess(false); setIsModalOpen(false); }, 3000);
+      setTimeout(() => { setIsSuccess(false); setIsModalOpen(false); }, 6000);
     } catch {
       alert("Something went wrong. Please try again.");
     } finally {
